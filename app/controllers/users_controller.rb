5e2a403734b688ba20320a2e8fb6e7b12a2a4500
Onur Kaya <onurkaya8@outlook.com>
2020-02-03 17:59:24 +0300

    form created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update]
+
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to root_path
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: [:show]
+
+  root "users#index"
+
+  get "/users", to: "users#index"
+  get "/new", to: "users#new"
+  get "/users/:id", to: "users#edit"
 end
